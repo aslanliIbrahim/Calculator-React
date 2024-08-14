6f3bb3c28c27a592ec24779ea4b942c6c939ca04
aslanliIbrahim <ibrahimra@code.edu.az>
2024-08-14 21:31:04 +0400

    structure is ready for use

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -10,7 +10,6 @@ function App() {
         <h1 className="text-center font-semibold text-4xl text-white">
           Calculator
         </h1>
-
         <div className="h-1/2 border border-separate flex flex-col justify-end items-end ">
           <input
             className="tracking-wider w-1/2 text-3xl outline-none text-right bg-transparent font-semibold"
@@ -18,6 +17,66 @@ function App() {
             value={result}
             disabled
           />
+        </div>
+        <div className="grid grid-cols-4 gap-2">
+          <button type="button" className="operator">
+            C
+          </button>
+          <button type="button" className="operator">
+            +/-
+          </button>
+          <button type="button" className="operator">
+            %
+          </button>
+          <button type="button" className="operator">
+            +
+          </button>
+          <button type="button" className="operator">
+            1
+          </button>
+          <button type="button" className="operator">
+            2
+          </button>
+          <button type="button" className="operator">
+            3
+          </button>
+          <button type="button" className="operator">
+            -
+          </button>
+          <button type="button" className="operator">
+            4
+          </button>
+          <button type="button" className="operator">
+            5
+          </button>
+          <button type="button" className="operator">
+            6
+          </button>
+          <button type="button" className="operator">
+            *
+          </button>
+          <button type="button" className="operator">
+            7
+          </button>
+          <button type="button" className="operator">
+            8
+          </button>
+          <button type="button" className="operator">
+            9
+          </button>
+          <button type="button" className="operator">
+            /
+          </button>
+          <button type="button" className="operator">
+            0
+          </button>
+          <button type="button" className="operator">
+            .
+          </button>
+          <button type="button" className="operator col-span-2">
+            =
+          </button>
+          
         </div>
       </div>
     </Background>
